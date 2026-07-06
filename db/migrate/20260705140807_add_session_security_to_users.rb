870acd8f39f02d733f7ed898816f379adbc85732
venu bhat <venubha147@gmail.com>
2026-07-06 14:33:35 +0530

    Test

--- db/migrate/20260705140807_add_session_security_to_users.rb
+++ db/migrate/20260705140807_add_session_security_to_users.rb
@@ -1,10 +1,14 @@
 class AddSessionSecurityToUsers < ActiveRecord::Migration[8.0]
+  class MigrationUser < ActiveRecord::Base
+    self.table_name = "users"
+  end
+
   def up
     add_column :users, :session_token, :string
     add_column :users, :suspended_at, :datetime
 
-    User.reset_column_information
-    User.find_each { |user| user.update_column(:session_token, SecureRandom.hex(32)) }
+    MigrationUser.reset_column_information
+    MigrationUser.find_each { |user| user.update_column(:session_token, SecureRandom.hex(32)) }
 
     change_column_null :users, :session_token, false
   end
